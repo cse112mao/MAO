b85f0cfc2d8c178be7c238decf7e964c88fa54ea
Matthew Wen <Matt@Matthews-MacBook-Pro.local>
2017-04-17 02:44:57 -0700

    test heroku deploy v2

--- doc/api_data.js
+++ doc/api_data.js
@@ -1,52 +1,1 @@
-define({ "api": [
-  {
-    "type": "get",
-    "url": "/hello",
-    "title": "Respond back name passed as query",
-    "name": "Ping_Name",
-    "parameter": {
-      "fields": {
-        "Parameter": [
-          {
-            "group": "Parameter",
-            "type": "String",
-            "optional": false,
-            "field": "name",
-            "description": "<p>of the user.</p>"
-          }
-        ]
-      }
-    },
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "Boolean",
-            "optional": false,
-            "field": "err",
-            "description": "<p>error if occured.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "message",
-            "description": "<p>status message.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "name",
-            "description": "<p>query name.</p>"
-          }
-        ]
-      }
-    },
-    "version": "0.0.0",
-    "filename": "routes/hello.js",
-    "group": "C__Users_Byungtastic_Desktop_MAO_Proj_routes_hello_js",
-    "groupTitle": "C__Users_Byungtastic_Desktop_MAO_Proj_routes_hello_js"
-  }
-] });
+define({ "api": [  {    "type": "get",    "url": "/hello",    "title": "Respond back name passed as query",    "name": "Ping_Name",    "parameter": {      "fields": {        "Parameter": [          {            "group": "Parameter",            "type": "String",            "optional": false,            "field": "name",            "description": "<p>of the user.</p>"          }        ]      }    },    "success": {      "fields": {        "Success 200": [          {            "group": "Success 200",            "type": "Boolean",            "optional": false,            "field": "err",            "description": "<p>error if occured.</p>"          },          {            "group": "Success 200",            "type": "String",            "optional": false,            "field": "message",            "description": "<p>status message.</p>"          },          {            "group": "Success 200",            "type": "String",            "optional": false,            "field": "name",            "description": "<p>query name.</p>"          }        ]      }    },    "version": "0.0.0",    "filename": "routes/hello.js",    "group": "_Users_Matt_Desktop_project_MAO_routes_hello_js",    "groupTitle": "_Users_Matt_Desktop_project_MAO_routes_hello_js"  }] });
--- doc/api_project.js
+++ doc/api_project.js
@@ -1,14 +1,1 @@
-define({
-  "name": "",
-  "version": "0.0.0",
-  "description": "",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2017-04-17T08:19:52.211Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.5"
-  }
-});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-04-17T09:42:19.588Z",    "url": "http://apidocjs.com",    "version": "0.17.5"  }});
