70ac817c686350098049c2729dae289366da3c7b
Minh Vu <mbvu@ucsd.edu>
2017-04-17 03:05:06 -0700

    updated package.json and readme for instructions on how to get code working on local machine

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MAO
 CSE 112 Project
+
+To get this working on your local machine, do the following steps:
+
+1. Clone this repo
+2. In the root directory of your repo, run "npm install"
--- doc/api_project.js
+++ doc/api_project.js
@@ -1,1 +1,14 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-04-17T09:42:19.588Z",    "url": "http://apidocjs.com",    "version": "0.17.5"  }});
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-04-17T10:03:29.348Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
