70a6f86777447897431133dcc163f69fd0a41480
yutong yang <yutongyang@yutongs-MacBook-Pro.local>
2017-04-21 10:47:00 -0700

    wait i messed up this is the real fix

--- views/hw2/hw2.js
+++ views/hw2/hw2.js
@@ -47,7 +47,7 @@ var formatTime = function formatTime(strInputTime, boolInputSeconds) {
 */
 var isValidTime = function isValidTime(strInputTime, boolInputSeconds) {
   // store the formatted input
-  var arrTime = formatTime(strInputTime, true);
+  var arrTime = formatTime(strInputTime, boolInputSeconds);
 
   // check to see if input was incorrect
   if (arrTime === null)
